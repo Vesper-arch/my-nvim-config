d480b866a5a8a4dd424b48210f9078dceec39779
Vesper-arch <ej1mahoney@gmail.com>
2024-02-26 20:14:58 -0500

    Add files via upload

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+
+return {
+  {
+    "catppuccin-mocha/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "catppuccin"
+    end
+  },
+}
