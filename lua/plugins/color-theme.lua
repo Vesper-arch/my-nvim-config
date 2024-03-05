f0124fe73961e206d3da4e8e2d097b9823f9487d
Vesper-arch <ej1mahoney@gmail.com>
2024-03-04 20:54:16 -0500

    Added formatting to some languages | is broken rn idk why

--- lua/plugins/color-theme.lua
+++ lua/plugins/color-theme.lua
@@ -1,21 +1,21 @@
 local catppuccin = {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "catppuccin"
-    end
+	"catppuccin/nvim",
+	lazy = false,
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
 
 local tokyonight = {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      vim.cmd.colorscheme "tokyonight"
-    end
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+	config = function()
+		vim.cmd.colorscheme("tokyonight")
+	end,
 }
 -- List of currently installed themes:
 -- Catppuccin
