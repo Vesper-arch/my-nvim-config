477a1da136a2da42fcb5771d3aec25e139ebb01a
Vesper-arch <ej1mahoney@gmail.com>
2024-03-05 19:57:58 -0500

    Added rudimentary code completion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -7,19 +7,26 @@ return {
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "pyright", "lua_ls", "rust_analyzer" },
-			})
-		end,
+        lazy = false,
+        opts = {
+            auto_install = true
+        }
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.pyright.setup({})
-			lspconfig.rust_analyzer.setup({})
+			lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+			lspconfig.pyright.setup({
+                capabilities = capabilities
+            })
+			lspconfig.rust_analyzer.setup({
+                capabilities = capabilities
+            })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
