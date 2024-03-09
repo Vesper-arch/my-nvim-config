0c541243e00867346e53c2abd917f9966d633604
Vesper-arch <ej1mahoney@gmail.com>
2024-03-09 11:13:17 -0500

    Fixed Python LSP support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,32 +1,54 @@
 return {
 	{
 		"williamboman/mason.nvim",
+		lazy = false,
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        opts = {
-            auto_install = true
-        }
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
-                capabilities = capabilities
-            })
-			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+            lspconfig.pyright.setup {
+                settings = {
+                    pyright = {
+                        -- Using Ruff's import organizer
+                        disableOrganizeImports = true,
+                    },
+                    python = {
+                        analysis = {
+                            -- Ignore all files for analysis to exclusively use Ruff for linting
+                            ignore = { '*' },
+                        },
+                    },
+                },
+            }
+            lspconfig.ruff_lsp.setup({
+                on_attach = on_attach,
+                init_options = {
+                    settings = {
+                        "line-length=300",
+                        "ignore=[E501, E261]"
+                    }
+                },
                 capabilities = capabilities
             })
 			lspconfig.rust_analyzer.setup({
-                capabilities = capabilities
-            })
+				capabilities = capabilities,
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
